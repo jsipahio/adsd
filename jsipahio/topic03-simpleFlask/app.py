0562e756d3a46d3cbc5c80a7e2f51970bfa74680
jsipahio <116314200+jsipahio@users.noreply.github.com>
2024-09-12 01:45:44 +0000

    added update and create routes

--- jsipahio/topic03-simpleFlask/app.py
+++ jsipahio/topic03-simpleFlask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -7,6 +7,7 @@ app = Flask(__name__)
 cnn = sqlite3.connect("pets.db", check_same_thread=False)
 
 
+@app.route("/")
 @app.route("/list")
 def list_pets():
     # crs = cnn.cursor()
@@ -17,18 +18,58 @@ def list_pets():
     print(rows)
     return render_template("list.html", prof={'name':"john", 'class':'ADSD'}, rows=rows)
 
+
 @app.route("/delete/<id>")
 def get_delete(id):
     crs = cnn.cursor()
     crs.execute("delete from pets where id=?", (id,))
     cnn.commit()
-    return f"<p>Trying to delete id = {id}</p>"
+    return redirect(url_for('list_pets',))
 
-@app.route("/")
+
 @app.route("/hello")
 def hello_world():
     return("<p>Hello there, World!</p>")
 
+
 @app.route("/goodbye")
 def goodbye():
     return ("<p>Goodbye, then! Have a nice day.")
+
+
+@app.route("/create", methods=['GET', 'POST'])
+def get_create():
+    if request.method == 'POST':
+        data = dict(request.form)
+        crs = cnn.cursor()
+        crs.execute("""
+        insert into pets(name, type, age, owner)
+        values (?,?,?,?)""",(data.get("name",""), data.get("type",""), int(data.get("age",0)), data.get("owner", ""),))
+        cnn.commit()
+        return redirect(url_for('list_pets',))
+    if request.method == 'GET':
+        return render_template("create.html")
+    
+
+@app.route("/update/<id>", methods=['GET'])
+def get_update(id):
+    crs = cnn.cursor()
+    crs.execute("select * from pets where id = ?", (id,))
+    data = crs.fetchone()
+    return render_template("update.html", data=data)
+
+
+@app.route("/update", methods=['POST'])
+def post_update():
+    print('hello')
+    crs = cnn.cursor()
+    try:
+        data = request.form
+    except:
+        return "Error updating pet. try again later"
+    crs.execute("""
+    update pets
+    set name = ?, type = ?, age = ?, owner = ?
+    where id = ?""", (data["name"], data["type"], int(data["age"]), data["owner"], int(data["id"])))
+    cnn.commit()
+    return redirect(url_for('list_pets'))
